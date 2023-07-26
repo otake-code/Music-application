3600f681ef354b44d803fa8f58d258e72c88c5e5
otake <0.28t@icloud.com>
2023-07-26 11:37:13 +0900

    7月26日のプッシュ

--- app/src/main/java/com/example/musicapplication/MainActivity.java
+++ app/src/main/java/com/example/musicapplication/MainActivity.java
@@ -5,22 +5,32 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CompoundButton;
+import android.widget.ImageButton;
 import android.widget.SeekBar;
+import android.widget.TextView;
 import android.widget.ToggleButton;
+import android.app.Activity;
 
 public class MainActivity extends AppCompatActivity {
 
     private MediaPlayer mediaPlayer;
-    private Button button_play;
+    private ImageButton button_play;
     private SeekBar speedSeekBar;
     private SeekBar pitchSeekBar;
     private ToggleButton toggleButton1;
     private ToggleButton toggleButton2;
     private ToggleButton toggleButton3;
     private ToggleButton toggleButton4;
+    private Button timerButton;
+    private TextView timerTextView;
+    private CountDownTimer timer; // timerを宣言
+    private static final int REQUEST_CODE_TIMER_SETTING = 1;
+    private long timerDuration = 60000; // デフォルトのタイマー時間（1分）
+    private long timeLeftInMillis = 0;
     private View graphView; // 棒グラフを表示するView
 
     @Override
@@ -39,6 +49,8 @@ public class MainActivity extends AppCompatActivity {
         button_play = findViewById(R.id.play_button);
         speedSeekBar = findViewById(R.id.speedSeekBar);
         pitchSeekBar = findViewById(R.id.pitchSeekBar);
+
+        timerButton =findViewById(R.id.btn_open_timer_setting);
         //graphView = findViewById(R.id.graph_view);
 
         toggleButton1.setTextOn("1");
@@ -49,6 +61,7 @@ public class MainActivity extends AppCompatActivity {
         toggleButton3.setTextOff("3");
         toggleButton4.setTextOn("4");
         toggleButton4.setTextOff("4");
+
 
 
         toggleButton1.setOnCheckedChangeListener((buttonView, isChecked) -> {
@@ -84,7 +97,147 @@ public class MainActivity extends AppCompatActivity {
             } else {}
         });
 
-        /*btn1.setOnClickListener(new View.OnClickListener() {
+
+
+
+        button_play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mediaPlayer != null) {
+                    if (mediaPlayer.isPlaying()) {
+                        mediaPlayer.pause();
+                        //button_play.setText("PLAY");
+                        // ボタンの背景画像を変更
+                        button_play.setBackgroundResource(R.drawable.start); // stop.jpegの画像を設定
+                        if(timer != null){
+                            timer.cancel();
+                        }
+                        //hideGraph(); // 再生を一時停止したら棒グラフも非表示にする
+                    } else {
+                        float speed = speedSeekBar.getProgress() / 100f;
+                        float pitch = pitchSeekBar.getProgress() / 100f;
+
+                        mediaPlayer.setPlaybackParams(mediaPlayer.getPlaybackParams().setSpeed(speed));
+                        mediaPlayer.setPlaybackParams(mediaPlayer.getPlaybackParams().setPitch(pitch));
+
+                        if (mediaPlayer.getCurrentPosition() == mediaPlayer.getDuration()) {
+                            mediaPlayer.seekTo(0);
+                        }
+                        mediaPlayer.start();
+                        //button_play.setText("STOP");
+                        // ボタンの背景画像を変更
+                        button_play.setBackgroundResource(R.drawable.stop); // play.jpegの画像を設定
+                        // タイマーを設定して開始
+                        if (timer != null) {
+                            timer.cancel();
+                        }
+                        //timerDuration = 60000; // タイマーの時間をミリ秒単位で設定（ここでは1分）
+                        startTimer(timerDuration);
+
+                        //showGraph(); // 再生開始時に棒グラフを表示する
+                    }
+                }
+            }
+        });
+
+        // TextViewを取得
+        timerTextView = findViewById(R.id.timerTextView);
+
+        // タイマーの時間を表示するメソッドを呼び出す
+        updateTimerText(timeLeftInMillis);
+
+        // タイマー設定画面を開くボタンのクリックリスナーを設定
+        timerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openTimerSettingActivity();
+            }
+        });
+    }
+    // タイマーの時間を表示するメソッド
+    private void updateTimerText(long milliseconds) {
+        int hours = (int) (milliseconds / 3600000);
+        int minutes = (int) ((milliseconds % 3600000) / 60000);
+        int seconds = (int) ((milliseconds % 60000) / 1000);
+        String timeString = String.format("%02d:%02d:%02d", hours, minutes, seconds);
+        timerTextView.setText(timeString);
+    }
+
+    /*private void startTimer(long duration) {
+        timer = new CountDownTimer(duration, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                // タイマーの残り時間が更新されたときの処理
+            }
+
+            @Override
+            public void onFinish() {
+                // タイマーが終了したときの処理
+                if (mediaPlayer != null && mediaPlayer.isPlaying()) {
+                    mediaPlayer.pause();
+                    button_play.setBackgroundResource(R.drawable.start);
+                }
+            }
+        }.start();
+    }*/
+    // タイマーを開始するメソッド
+    private void startTimer(long durationInMillis) {
+        if (timer != null) {
+            timer.cancel();
+        }
+
+        timer = new CountDownTimer(durationInMillis, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                // 残り時間を表示
+                updateTimerText(millisUntilFinished);
+            }
+
+            @Override
+            public void onFinish() {
+                // タイマーが終了したときの処理
+                updateTimerText(0);
+                if (mediaPlayer != null && mediaPlayer.isPlaying()) {
+                    mediaPlayer.pause();
+                    button_play.setBackgroundResource(R.drawable.start);
+                }
+            }
+        }.start();
+    }
+
+
+    private void openTimerSettingActivity() {
+        Intent intent = new Intent(this, TimeSettingActivity.class);
+        startActivityForResult(intent, REQUEST_CODE_TIMER_SETTING);
+    }
+
+    // タイマー設定画面から戻ってきたときに呼ばれるメソッド
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CODE_TIMER_SETTING && resultCode == Activity.RESULT_OK) {
+            if (data != null) {
+                // 設定されたタイマー時間を取得
+                long selectedDuration = data.getLongExtra("timer_duration", 0);
+                if (selectedDuration > 0) {
+                    timerDuration = selectedDuration;
+                }
+            }
+        }
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (mediaPlayer != null) {
+            mediaPlayer.release();
+            mediaPlayer = null;
+        }
+    }
+}
+
+ /*btn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 // ボタン1がクリックされたときの処理
@@ -112,88 +265,3 @@ public class MainActivity extends AppCompatActivity {
                 mediaPlayer = MediaPlayer.create(MainActivity.this, R.raw.music4);
             }
         });*/
-
-        button_play.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (mediaPlayer != null) {
-                    if (mediaPlayer.isPlaying()) {
-                        mediaPlayer.pause();
-                        button_play.setText("PLAY");
-                        //hideGraph(); // 再生を一時停止したら棒グラフも非表示にする
-                    } else {
-                        float speed = speedSeekBar.getProgress() / 100f;
-                        float pitch = pitchSeekBar.getProgress() / 100f;
-
-                        mediaPlayer.setPlaybackParams(mediaPlayer.getPlaybackParams().setSpeed(speed));
-                        mediaPlayer.setPlaybackParams(mediaPlayer.getPlaybackParams().setPitch(pitch));
-
-                        if (mediaPlayer.getCurrentPosition() == mediaPlayer.getDuration()) {
-                            mediaPlayer.seekTo(0);
-                        }
-                        mediaPlayer.start();
-                        button_play.setText("STOP");
-                        //showGraph(); // 再生開始時に棒グラフを表示する
-                    }
-                }
-            }
-        });
-
-    }
-
-    //======================グラフの描画===============
-    /*private void showGraph() {
-        if (!isGraphShowing) {
-            isGraphShowing = true;
-            graphView.setVisibility(View.VISIBLE);
-            startGraphAnimation();
-        }
-    }
-
-    private void hideGraph() {
-        if (isGraphShowing) {
-            isGraphShowing = false;
-            graphView.setVisibility(View.INVISIBLE);
-            stopGraphAnimation();
-        }
-    }
-
-    private void startGraphAnimation() {
-        final int maxGraphHeight = graphView.getHeight(); // グラフの最大の高さをViewの高さとする
-        final Random random = new Random();
-        final Runnable graphAnimationRunnable = new Runnable() {
-            @Override
-            public void run() {
-                if (isGraphShowing) {
-                    int graphHeight = random.nextInt(maxGraphHeight);
-                    updateGraphView(graphHeight);
-                    graphView.postDelayed(this, 100); // 100msごとに新しいランダムな高さの棒グラフを描画
-                }
-            }
-        };
-        graphAnimationRunnable.run();
-    }
-
-    private void stopGraphAnimation() {
-        graphView.removeCallbacks(null);
-    }
-
-    private void updateGraphView(int graphHeight) {
-        // ここで実際の棒グラフの描画処理を行う
-        // graphHeightを利用して、表示を更新する
-        // この例では単純に高さを設定していますが、実際の描画は要件に合わせてカスタマイズしてください
-        graphView.getLayoutParams().height = graphHeight;
-        graphView.requestLayout();
-    }*/
-//===== 終わり============================---
-
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        if (mediaPlayer != null) {
-            mediaPlayer.release();
-            mediaPlayer = null;
-        }
-    }
-}
---
+++ app/src/main/java/com/example/musicapplication/TimeSettingActivity.java
@@ -0,0 +1,60 @@
+package com.example.musicapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.NumberPicker;
+
+public class TimeSettingActivity extends AppCompatActivity {
+
+    private NumberPicker hourPicker;
+    private NumberPicker minutePicker;
+    private NumberPicker secondPicker;
+    private Button btnOk;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_time_setting);
+
+        hourPicker = findViewById(R.id.hour_picker);
+        minutePicker = findViewById(R.id.minute_picker);
+        secondPicker = findViewById(R.id.second_picker);
+        btnOk = findViewById(R.id.btn_ok);
+        // 最大、最小を設定
+        hourPicker.setMaxValue(23);
+        hourPicker.setMinValue(0);
+
+        // 最大、最小を設定
+        minutePicker.setMaxValue(59);
+        minutePicker.setMinValue(0);
+        // 最大、最小を設定
+        secondPicker.setMaxValue(59);
+        secondPicker.setMinValue(0);
+
+        // NumberPickerやButtonなどのUI要素の初期化やリスナーの設定などを行う
+
+        btnOk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // OKボタンが押されたときの処理
+
+                int selectedHours = hourPicker.getValue();
+                int selectedMinutes = minutePicker.getValue();
+                int selectedSeconds = secondPicker.getValue();
+                long timerDuration = (selectedHours * 3600 + selectedMinutes * 60 + selectedSeconds) * 1000; // 時間をミリ秒に変換
+
+                Intent intent = new Intent();
+                intent.putExtra("timer_duration", timerDuration);
+                setResult(Activity.RESULT_OK, intent);
+                finish();
+            }
+        });
+    }
+}
